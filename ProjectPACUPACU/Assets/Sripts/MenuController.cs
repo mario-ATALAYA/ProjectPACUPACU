00a3a595f5175aa7094c14911ab88b5afbc97908
Nagore-ATALAYA <45049372+Nagore-ATALAYA@users.noreply.github.com>
2018-11-15 16:53:18 +0100

    UI_Basica

---
+++ ProjectPACUPACU/Assets/Sripts/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject OptionsMenu;
+
+    private void Start()
+    {
+        OptionsMenu.SetActive(false);
+    }
+    public void PlayBtn()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OptionsBtn()
+    {
+        OptionsMenu.SetActive(true);
+    }
+
+    public void ExitBtn()
+    {
+        Application.Quit();
+    }
+    public void CloseOptionMenu()
+    {
+        OptionsMenu.SetActive(false);
+    }
+}
---
+++ ProjectPACUPACU/Assets/Sripts/SplashScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    private float ChangeSceneCont;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeSceneCont += Time.deltaTime;
+        if (ChangeSceneCont > 2)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
