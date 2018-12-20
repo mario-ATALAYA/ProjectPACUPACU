e99c66bddbc8f863504995adaac7fb8aa9ed6271
Aitor <45049374+Aitor-ATALAYA@users.noreply.github.com>
2018-12-20 18:32:55 +0100

    base

---
+++ ProjectPACUPACU/Assets/Sripts/Enemy Scripts/DetectionZone.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionZone : MonoBehaviour {
+    GameObject Player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public GameObject getPlayer()
+    {
+        return Player;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Player = other.gameObject;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.Equals(Player))
+        {
+            Player = null;
+        }
+    }
+}
---
+++ ProjectPACUPACU/Assets/Sripts/Enemy Scripts/EnemyR.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyR : MonoBehaviour {
+    public enum EnemyState
+    {
+        CHASE, PATROLL,WAIT
+    }
+    public EnemyState State;
+    Transform destination;
+   NavMeshAgent navMeshAgent;
+    public GameObject plane;
+    GameObject Player,detectionZone;
+    Vector3 targetVector;
+    // Use this for initialization
+    void Start () {
+        detectionZone = this.transform.GetChild(0).gameObject;
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+        if (navMeshAgent!= null)
+        {
+            setDestination();
+        }
+        else
+        {
+            print("no tengo NavMes:" + gameObject.name);
+        }
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (detectionZone != null)
+        {
+            Player = detectionZone.GetComponent<DetectionZone>().getPlayer();
+        }
+        else
+        {
+            detectionZone = this.transform.GetChild(0).gameObject;
+        }
+       if (Player != null)
+        {
+           State= EnemyState.CHASE;
+        }
+        else
+        {
+            State = EnemyState.PATROLL;
+        }
+        switch (State)
+        {
+            case EnemyState.CHASE:
+                if (Player != null)
+                {
+
+                }
+               
+                break;
+            case EnemyState.PATROLL:
+              
+            if(  navMeshAgent.remainingDistance<1)
+                {
+                    print("toy cerca");
+                    targetVector = randomVector();
+                }
+                navMeshAgent.SetDestination(targetVector);
+                break;
+
+        }
+	}
+    private void setDestination()
+    {
+        
+        
+        Vector3 targetVector;
+       
+        if (destination != null)
+        {
+             targetVector = destination.transform.position;
+           
+        }
+        
+       
+    }
+    private Vector3 randomVector()
+    {
+        Vector3 a=new Vector3();
+        float x, z;
+        x = Random.Range(-plane.GetComponent<Renderer>().bounds.extents.magnitude, plane.GetComponent<Renderer>().bounds.extents.magnitude);
+        z= Random.Range(-plane.GetComponent<Renderer>().bounds.extents.magnitude, plane.GetComponent<Renderer>().bounds.extents.magnitude);
+       // x = Random.Range(-5.5f, 5.5f);
+      //  z = Random.Range(-5.5f, 5.5f);
+
+        a = new Vector3(x, .5f,z);
+
+        return a;
+    }
+}
